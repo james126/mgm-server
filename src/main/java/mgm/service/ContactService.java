49f79865c94482a0bf5871ddc2062735cefea7bf
James <44129935+james126@users.noreply.github.com>
2023-08-02 14:41:19 +1200

    Update README.md

--- src/main/java/mgm/service/ContactService.java
+++ src/main/java/mgm/service/ContactService.java
@@ -8,26 +8,12 @@ import java.util.Optional;
 public interface ContactService {
 
     void insertContact(Contact form);
-    //Test ok
-    //Test when form is null
-    //Test when form non fields are null
-    //tet when fields are incompatible types
 
     void deleteById(Integer id);
-    //delete by id
-    //delete by null
-    //delete by string
-    //delete by id doesnt exist
 
     Optional<Contact> findByMinId();
-    //find by min id
-    //find by min id when database is blank
 
     List<Contact> getAllContacts();
 
     Optional<Contact> getNextContactForm(Integer id);
-    //find by id
-    //find by null
-    //find by string
-    //find by id doesnt exist
 }
